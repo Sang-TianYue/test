2db492c171dd2051ded8ed56d1a8fd010a7dd1f4
Sang-TianYue <lisy_program@163.com>
2019-03-20 22:19:00 +0800

    add a project about scrapy shiyanlou repo

--- github-repo/github/github/items.py
+++
@@ -1,9 +0,0 @@
-
-import scrapy
-
-
-class GithubItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    name = scrapy.Field()
-    update_time = scrapy.Field()
