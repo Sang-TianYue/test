b0ef8a328f663ba2617435bff4457518c197b3d5
Sang-TianYue <lisy_program@163.com>
2019-05-17 13:33:52 +0800

    addition

--- MySelenium/baseInfo/__init__.py
+++ MySelenium/baseInfo/__init__.py
@@ -7,4 +7,4 @@ paramter about send email
 mail_server = 'smtp.163.com'
 mail_send = 'lisy_program@163.com'
 mail_password = 'aaa111'
-mail_receiver = ['lisy_from@163.com', 'lisy_program@163.com']
+mail_receiver = ['lisy_from@163.com', 'lisy_program@163.com', 'lisy@jjmmw.com', 'qiujj@jjmmw.com']
